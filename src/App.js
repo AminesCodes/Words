cd92ff8c94f5b122ad07986dec54ff0de2ef4956
AminesCodes <aminebensalem@pursuit.org>
2020-08-13 22:19:53 -0400

    Switch the word selection logic to a random word

--- src/App.js
+++ src/App.js
@@ -3,34 +3,34 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [words, setWords] = useState([]);
   const [input, setInput] = useState('');
-  const [index, setIndex] = useState(0);
+  const [targetWord, setTargetWord] = useState('');
   const [submitted, setSubmitted] = useState(false);
 
-  const decreaseIndex = () => {
-    let targetIndex = index - 1;
-    if (targetIndex < 0) {
-      targetIndex = words.length - 1;
+  const nextWord = (list) => {
+    if (!list.length) {
+      setSubmitted(false);
+      setTargetWord('');
+    } else {
+      const listCopy = [...list];
+      const randomIndex = Math.floor(Math.random() * list.length);
+      const randomWord = listCopy.splice(randomIndex, 1)[0];
+      setTargetWord(randomWord);
+      setWords(listCopy);
     }
-    setIndex(targetIndex);
-  }
-
-  const increaseIndex = () => {
-    let targetIndex = index + 1;
-    if (targetIndex >= words.length) {
-      targetIndex = 0;
-    }
-    setIndex(targetIndex);
   }
 
   const handleSubmit = () => {
     if (input.length) {
-      setWords(input.split(','));
+      const wordsList = input.split(',')
+      setWords(wordsList);
       setInput('');
       setSubmitted(true);
+      nextWord(wordsList);
     } else {
       window.alert('Pour continuer, la case mots doit avoir un contenu')
     }
   }
+
 
   useEffect(() => {
 
@@ -42,9 +42,8 @@ function App() {
       {
         submitted
         ? <>
-            <div>{words[index].trim()}</div>
-            <button onClick={decreaseIndex}>Précédent</button>
-            <button onClick={increaseIndex}>Suivant</button>
+            <p>{targetWord.trim()}</p>
+            <button onClick={() => nextWord(words)}>Suivant</button>
           </>
         : <>
             <label>
